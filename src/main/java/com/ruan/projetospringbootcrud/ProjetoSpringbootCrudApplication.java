c4e9d5d44fcc4713f2f66b2398d1486f7b8d35c4
Ruan Costa Oliveira <umenorin@gmail.com>
2023-09-22 16:48:09 -0300

    initializing a spring boot project

---
+++ src/main/java/com/ruan/projetospringbootcrud/ProjetoSpringbootCrudApplication.java
@@ -0,0 +1,13 @@
+package com.ruan.projetospringbootcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringbootCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringbootCrudApplication.class, args);
+	}
+
+}
